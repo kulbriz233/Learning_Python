9fd15b4d504d9f1b435b585b2175b757f87c2dfb
Augustine Ninyung <92556407+kulbriz233@users.noreply.github.com>
2024-11-13 15:21:13 +0000

    stringindexing

---
+++ Python_basics/string_indexing.py
@@ -0,0 +1,6 @@
+sentence = "Python is powerful and easy to learn."
+
+print("Length of the string:", len(sentence))
+print("First character:", sentence[0])
+print("Last character:", sentence[-1])
+print("Word 'powerful':", sentence[10:18])
