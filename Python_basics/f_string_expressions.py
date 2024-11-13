4568183fc6cb32654a46a5647696151606a0617f
Augustine Ninyung <92556407+kulbriz233@users.noreply.github.com>
2024-11-13 15:24:00 +0000

    f_string_expressions

---
+++ Python_basics/f_string_expressions.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+
+sum_result = f'The sum of the two variables are {a + b}'
+product_result = f'The product of the two variables are {a * b}'
+difference_result = f'The difference between the two variables are {a - b}'
+print(sum_result)
+print(product_result)
+print(difference_result)
