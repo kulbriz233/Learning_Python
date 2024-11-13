d6895d70ec452f47d5a7646e7a3fa64de4bf9dc5
Augustine Ninyung <92556407+kulbriz233@users.noreply.github.com>
2024-11-13 15:10:47 +0000

    Stringmethods

---
+++ Python_basics/stringmethods.py
@@ -0,0 +1,8 @@
+name = "Santiago"
+region = "North"
+about_me = ["I like to play game a lot", "I like to read as well", "I cook a lot too"]
+
+print(name.upper())
+print(name.lower())
+print(region.replace('North','Upper West'))
+print(', '.join(about_me))
